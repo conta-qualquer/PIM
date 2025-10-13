f2dff26e0710e018c8b229a0be10e74036141db0
conta-qualquer <conta_234a@proton.me>
2025-10-13 00:36:44 -0300

    menus.py

---
+++ code/menus.py
@@ -0,0 +1,33 @@
+def menu_principal():
+    print("\n\n------------Bem Vindo ao Menu Principal------------")
+    print("\n1-Gerenciar Alunos")
+    print("2-Gerenciar Turmas")
+    print("3-Gerenciar Aulas")
+    print("4-Gerenciar Atividades")
+    print("5-Inteligencia Artificial")
+    print("6-Relatorios e Consultas")
+    print("7-sair")
+
+def menu_aluno():
+    print("\n----------GERENCIAR ALUNO----------")
+    print("\n1-Cadastrar Aluno")
+    print("2-Listar Alunos")
+    print("3-Buscar Aluno")
+
+def menu_turma():
+    print("\n----------GERENCIAR TURMA----------")
+    print("\n1-Cadastrar Turma")
+    print("2-Listar Turmas")
+    print("3-Ver alunos da Turma")
+
+def menu_aulas():
+    print("\n----------GERENCIAR AULAS----------")
+    print("\n1-Registrar Aulas")
+    print("2-Listar Aulas de uma Turma")
+    print("3-Consulta aulas por Data")
+
+def menu_atividade():
+    print("\n----------GERENCIAR ATIVIDADE----------")
+    print("\n1-Cadastrar Atividade")    
+    print("2-Listar Atividade")    
+    print("3-Consulta Atividade por Turma")
