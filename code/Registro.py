299484c04aa60976ef1f261f6480e0e4705780e6
conta-qualquer <conta_234a@proton.me>
2025-10-13 00:39:17 -0300

    registro_aula.py

--- code/Registro.py
+++
@@ -1,8 +0,0 @@
-class RegistroAula:
-    def __init__(self , data , tema , turma):
-        self.data = data
-        self.tema = tema
-        self.turma = turma
-    
-    def exibir_registro(self):
-       pass
