cc8eb85931a3e8714eaa740eded285f3684c9c41
conta-qualquer <conta_234a@proton.me>
2025-10-14 17:24:55 -0300

    Delete code directory

--- code/Turma.py
+++
@@ -1,8 +0,0 @@
-class Turma:
-    def __init__(self , nome , semestre , curso):
-        self.nome = nome
-        self.semestre = semestre
-        self.curso = curso
-        
-    def __str__(self):
-        return 'Turma : {} | Semestre :  {} | Curso : {}'.format(self.nome,self.semestre,self.curso)
--- code/atividade.py
+++
@@ -1,8 +0,0 @@
-class Atividade():
-    def __init__(self ,nome,data,turma):
-        self.nome = nome
-        self.data = data
-        self.turma = turma
-    
-    def __str__(self) -> str:
-        return 'Nome da Atividade : {} | Data da Atividade : {} | Turma : {}'.format(self.nome,self.data,self.turma)
