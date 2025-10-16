ba56a78ec82afe238db2ca36cec4faa98e921e7d
conta-qualquer <conta_234a@proton.me>
2025-10-15 23:39:52 -0300

    Delete codigo directory

--- codigo/atividade.py
+++
@@ -1,8 +0,0 @@
-class Atividade:
-    def __init__(self , nome , data , turma):
-        self.nome = nome 
-        self.data = data
-        self.turma = turma
-    
-    def __str__(self) -> str:
-        return f'Nome da Atividade : {self.nome} | Data da Atividade : {self.data} | Turma : {self.turma}'
