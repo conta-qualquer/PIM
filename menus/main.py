fd7ffd6edaec20193eb6c33b06b2f4c59870955f
conta-qualquer <conta_234a@proton.me>
2025-10-25 10:03:23 -0300

    Create main.py

---
+++ menus/main.py
@@ -0,0 +1,37 @@
+from funcoes import ler_texto , limpar_tela , continuar
+from menus import * 
+
+
+def sistema_academico():
+
+    carregar_dados()
+    salvar_dados()
+
+    while True:
+        limpar_tela()
+        menu_principal()
+
+        opcoes = {
+            '1' : menu_principal_aluno,
+            '2' : menu_principal_turma,
+            '3' : menu_aula_principal,
+            '4' : menu_principal_atividade,
+            '5' : menu_princiapal_ia,
+            '6' : menu_principal_relatorio,
+            '7' : encerrar
+        }
+
+        opcao = ler_texto()
+        funcao = opcoes.get(opcao)
+        if funcao:
+            funcao()
+            if opcao == '7':
+                break
+            if continuar():
+                print("\nPROGRAMA ENCERRADO\n")
+                break
+        else:
+            print("\nDigite um valor válido\n")
+
+if __name__ == "__main__":
+    sistema_academico()
